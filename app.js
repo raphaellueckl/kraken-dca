87ff98606ea46d189175735d9026da0db487fe8e
Raphael Lueckl (taalura5) <raphael.lueckl1@swisscom.com>
2022-04-01 14:56:24 +0200

    cleanup & refactor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /**
  * Kraken DCA
  * by @codepleb
@@ -18,12 +19,9 @@
  * - Leave the script running for as long as you want to keep buying as often as possible. :)
  */
 
-// User defined
-const DATE_OF_CASH_REFILL = 26; // Day of month, where new funds get deposited regularly (ignore weekends, that will be handled automatically)
-const CURRENCY = "CHF"; // Swiss Francs for me. Choose the currency that you are depositing regularly. Check here how you currency has to be named: https://docs.kraken.com/rest/#operation/getAccountBalance
+const DATE_OF_CASH_REFILL = Number(process.env.DATE_OF_CASH_REFILL) || 26; // Day of month, where new funds get deposited regularly (ignore weekends, that will be handled automatically)
+const CURRENCY = process.env.CURRENCY || "CHF"; // Swiss Francs for me. Choose the currency that you are depositing regularly. Check here how you currency has to be named: https://docs.kraken.com/rest/#operation/getAccountBalance
 const KRAKEN_MIN_BTC_ORDER_SIZE = 0.0001; // Kraken currently has a minimum order size of 0.0001 BTC. Can be changed, but should be the standard for the next few years I think.
-
-// 🛑 🛑 🛑 Do not touch everything below here... 🛑 🛑 🛑
 
 const KRAKEN_API_PUBLIC_KEY = process.env.KRAKEN_API_PUBLIC_KEY; // Kraken API public key
 const KRAKEN_API_PRIVATE_KEY = process.env.KRAKEN_API_PRIVATE_KEY; // Kraken API private key
@@ -32,44 +30,16 @@ const https = require("https");
 
 const isWeekend = (date) => date.getDay() % 6 == 0;
 
-let now = new Date();
-let nextFiatDropDate = new Date(
-  `${now.getFullYear()}-${now.getMonth() + 1}-${DATE_OF_CASH_REFILL}`
-);
-if (nextFiatDropDate < now) {
-  nextFiatDropDate.setDate(1); //Needed because later used 'setMonth' has a weird implementation logic.
-  nextFiatDropDate.setMonth(nextFiatDropDate.getMonth() + 1);
-  nextFiatDropDate.setDate(DATE_OF_CASH_REFILL + 1); // We add 1 to make sure we don't run out of fiat in the end. This will set the date right to the start of the next day.
-}
+const publicApiPath = "/0/public/";
+const privateApiPath = "/0/private/";
 
-if (isWeekend(nextFiatDropDate)) nextFiatDropDate.setDate(nextFiatDropDate.getDate() + 1)
-// If first time was SA, next day will be SU, so we have to repeat the check.
-if (isWeekend(nextFiatDropDate)) nextFiatDropDate.setDate(nextFiatDropDate.getDate() + 1)
-
-const millisUntilNextFiatDrop = nextFiatDropDate - now;
-
-async function QueryPublicEndpoint(endPointName, inputParameters) {
-  let jsonData;
-  const baseDomain = "https://api.kraken.com";
-  const publicPath = "/0/public/";
-  const apiEndpointFullURL =
-    baseDomain + publicPath + endPointName + "?" + inputParameters;
-
-  const options = {
-    hostname: "api.kraken.com",
-    port: 443,
-    path: `${publicPath}${endPointName}?${inputParameters || ""}`,
-    method: "GET",
-  };
-
+const getRequest = async (options) => {
   const data = await new Promise((resolve, reject) => {
     const req = https.request(options, (res) => {
       let data = "";
-
       res.on("data", (d) => {
         data += d;
       });
-
       res.on("end", () => {
         resolve(data);
       });
@@ -78,31 +48,36 @@ async function QueryPublicEndpoint(endPointName, inputParameters) {
     req.on("error", (error) => {
       console.error(error);
     });
-
     req.end();
   });
+
+  return data;
+}
+
+const queryPublicApi = async (endPointName, inputParameters) => {
+  const options = {
+    hostname: "api.kraken.com",
+    port: 443,
+    path: `${publicApiPath}${endPointName}?${inputParameters || ""}`,
+    method: "GET",
+  };
+
+  const data = await getRequest(options);
 
   return JSON.parse(data);
 }
 
 async function QueryPrivateEndpoint(
-  endPointName,
-  inputParameters,
-  apiPublicKey,
-  apiPrivateKey
+  endpoint,
+  params,
 ) {
-  // const baseDomain = "https://api.kraken.com";
-  const privatePath = "/0/private/";
-
-  // const apiEndpointFullURL =
-  //   baseDomain + privatePath + endPointName + "?" + inputParametsers;
   const nonce = Date.now().toString();
-  const apiPostBodyData = "nonce=" + nonce + "&" + inputParameters;
+  const apiPostBodyData = "nonce=" + nonce + "&" + params;
 
   const signature = CreateAuthenticationSignature(
-    apiPrivateKey,
-    privatePath,
-    endPointName,
+    KRAKEN_API_PRIVATE_KEY,
+    privateApiPath,
+    endpoint,
     nonce,
     apiPostBodyData
   );
@@ -112,11 +87,11 @@ async function QueryPrivateEndpoint(
     const options = {
       hostname: "api.kraken.com",
       port: 443,
-      path: `${privatePath}${endPointName}${
-        inputParameters ? `?${inputParameters}` : ""
+      path: `${privateApiPath}${endpoint}${
+        params ? `?${params}` : ""
       }`,
       method: "POST",
-      headers: { "API-Key": apiPublicKey, "API-Sign": signature },
+      headers: { "API-Key": KRAKEN_API_PUBLIC_KEY, "API-Sign": signature },
     };
 
     
@@ -169,8 +144,6 @@ const executeBuyOrder = async () => {
   privateResponse = await QueryPrivateEndpoint(
     privateEndpoint,
     privateInputParameters,
-    KRAKEN_API_PUBLIC_KEY,
-    KRAKEN_API_PRIVATE_KEY
   );
   console.log(privateResponse);
 }
@@ -189,63 +162,55 @@ const Main = async () => {
     console.log();
     console.log("DCA activated now!");
 
-    if (1 == 1) {
+    const runCycle = async () => {
       // executeBuyOrder();
 
       let btcFiatPrice = (
-        await QueryPublicEndpoint("Ticker", `pair=xbt${CURRENCY.toLowerCase()}`)
+        await queryPublicApi("Ticker", `pair=xbt${CURRENCY.toLowerCase()}`)
       ).result[`XBT${CURRENCY}`].p[0];
       console.log(`BTC-Price: ${btcFiatPrice}`);
 
       let privateEndpoint = "Balance";
       let privateInputParameters = "";
 
-      privateResponse = (
+      const balance = (
         await QueryPrivateEndpoint(
           privateEndpoint,
           privateInputParameters,
-          KRAKEN_API_PUBLIC_KEY,
-          KRAKEN_API_PRIVATE_KEY
         )
       ).result;
 
-      const fiatAmount = privateResponse[CURRENCY];
+      const now = new Date();
+      const nextFiatDropDate = new Date(
+        `${now.getFullYear()}-${now.getMonth() + 1}-${DATE_OF_CASH_REFILL}`
+      );
+      if (nextFiatDropDate < now) {
+        nextFiatDropDate.setDate(1); //Needed because later used 'setMonth' has a weird implementation logic.
+        nextFiatDropDate.setMonth(nextFiatDropDate.getMonth() + 1);
+        nextFiatDropDate.setDate(DATE_OF_CASH_REFILL + 1); // We add 1 to make sure we don't run out of fiat in the end. This will set the date right to the start of the next day.
+      }
+      
+      if (isWeekend(nextFiatDropDate)) nextFiatDropDate.setDate(nextFiatDropDate.getDate() + 1)
+      // If first time was SA, next day will be SU, so we have to repeat the check.
+      if (isWeekend(nextFiatDropDate)) nextFiatDropDate.setDate(nextFiatDropDate.getDate() + 1)
+      
+      const millisUntilNextFiatDrop = nextFiatDropDate - now;
+      const fiatAmount = balance[CURRENCY];
       const myFiatValueInBtc = btcFiatPrice / fiatAmount;
       const approximatedAmoutOfOrdersUntilFiatRefill = myFiatValueInBtc / KRAKEN_MIN_BTC_ORDER_SIZE;
       const timeUntilNextOrderExecuted = millisUntilNextFiatDrop / approximatedAmoutOfOrdersUntilFiatRefill;
       const exactTimeDelayUntilNextOrder = now + timeUntilNextOrderExecuted;
 
       console.log("Current Price:", fiatAmount);
+
+      setTimeout(runCycle, exactTimeDelayUntilNextOrder);
     }
 
-    /*
-     * PRIVATE REST API Examples
-     */
+    runCycle();
 
-    if (1 == 0) {
-      let btcchfPrice = await QueryPublicEndpoint("Ticker", "pair=xbtchf");
-      
-
-      const EAO = 0; // Estimated Amount of Orders
-
-      let privateResponse = "";
-
-      let privateEndpoint = "AddOrder";
-      let privateInputParameters =
-        "pair=xbtchf&type=buy&ordertype=market&volume=5000";
-
-      privateResponse = await QueryPrivateEndpoint(
-        privateEndpoint,
-        privateInputParameters,
-        KRAKEN_API_PUBLIC_KEY,
-        KRAKEN_API_PRIVATE_KEY
-      );
-      console.log(privateResponse);
-    }
-
-    console.log("|=======================================|");
-    console.log("|             DCA stopped!              |");
-    console.log("|=======================================|");
+    // console.log("|=======================================|");
+    // console.log("|             DCA stopped!              |");
+    // console.log("|=======================================|");
   } catch (e) {
     console.log();
     console.log("AN EXCEPTION OCCURED :(");
